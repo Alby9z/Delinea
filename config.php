9a9fc0970ab5d17076ed0f5695b7fd51a9c3d16d
Xaluks <165258639+Xaluks@users.noreply.github.com>
2024-10-05 21:51:01 +0200

    test

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 // config.php
-define('SMTP_USERNAME', 'info_contact@xn--dlin-massage-bkb.fr');
+define('SMTP_USERNAME', 'info_contact@xn--dlina-massages-bkbd.fr');
 define('SMTP_PASSWORD', 'kz9viQZv7@ZDhUT');
